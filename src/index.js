1eb7d844c1b722ddb412d6e5bc742f1041c82aee
Anhlh19079 <Anhlh.nde19079@vtc.edu.vn>
2021-11-17 00:07:54 +0700

    add folder

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
+const express = require('express')
 const morgan = require('morgan')
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express();
 const port = 3000
-app = express();
 
+// HTTP loger
 app.use(morgan('combined'))
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.set("views", "./views");
+// Template engine
+var hbs = handlebars.create({
+    extname: '.hbs',
+    helpers: {
+        sayHello: function() { alert("Hello World") },
+        getStringifiedJson: function(value) {
+            return JSON.stringify(value);
+        }
+    },
+    defaultLayout: 'main',
+    // partialsDir: ['views/partials/'],
+});
+
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-});
+
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-//Template engine handlebars
